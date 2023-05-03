ddd62389e2986bbc49aa9f148e06a42e9cb9d6cf
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-05-03 17:05:15 +0530

    Now stores data in a JSON file

--- words.py
+++ words.py
@@ -1,11 +1,16 @@
+import json
+
 Intro = "Enter the list of words and press ctrl+D to stop."
 print(Intro)
 
-with open("words.txt", "a") as file:
-    while True:
-        try:
-            word = input("Word: ")
-            file.write(word+"\n")
-        except EOFError:
-            print()
-            break
+words = {}
+
+while True:
+    try:
+        words[input("Word: ")] = ""
+    except EOFError:
+        print()
+        break
+
+with open("crossword.json", "w") as file:
+    json.dump(words, file)
