df6572bd637f4affd589c4048cc1a6f839bca2f9
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-05-03 16:24:29 +0530

    Now uses for loop to generate words

--- generate.py
+++ generate.py
@@ -16,29 +16,21 @@ with open("api_key.txt") as file:
 
 openai.api_key = api_key
 
-prompt = f"suggest {n} words"
+for i in range(n):
+    prompt = f"generate one word"
 
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.8,
-    max_tokens=100,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0
-)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
 
-raw_answer = str(response["choices"][0]["text"])
-
-answer = raw_answer[2:]
-
-final_answer = ""
-for character in answer:
-    if character.isdigit() or character == "." or character == " ":
-        ...
-    else:
-        final_answer = final_answer + character
-
-
-with open("words.txt", "w") as file:
-    file.write(final_answer)
+    raw_answer = str(response["choices"][0]["text"])
+    print(raw_answer[2:])
+    with open("words.txt", "a", newline="") as file:
+        raw_answer = raw_answer.replace('\n','') 
+        file.write(raw_answer + '\n')
