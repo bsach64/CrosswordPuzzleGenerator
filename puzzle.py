99eae8014880ce7de4fa9a5019a65e77bf277295
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-05-05 15:40:28 +0530

    Places first word in a 25x25 board

--- puzzle.py
+++ puzzle.py
@@ -1,5 +1,6 @@
 import json
 
+board = [[None for i in range(25)] for j in range(25)]
 words = []
 
 with open("crossword.json") as file: 
@@ -7,4 +8,20 @@ with open("crossword.json") as file:
     crossword = json.load(file)
     for word in crossword:
         words.append(word)
+
+words = sorted(words, key=len, reverse=True)
+placed_words = []
+first_word = words[0]
+placed_words.append(first_word)
+filled_indexes = []
+
+start_index = 13 - (len(first_word) // 2)
+
+for character in first_word:
+    board[13][start_index] = character
+    filled_indexes.append((13, start_index))
+    start_index += 1
+
+print(board)
+
 
