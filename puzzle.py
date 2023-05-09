15d3774c54ca1c51bff1e43ca74e6382c2d2e358
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-05-09 19:44:07 +0530

    Structured the Code

--- puzzle.py
+++ puzzle.py
@@ -1,27 +1,49 @@
 import json
 
-board = [[None for i in range(25)] for j in range(25)]
-words = []
 
-with open("crossword.json") as file: 
-    crossword = {}
-    crossword = json.load(file)
-    for word in crossword:
-        words.append(word)
+def main():
+    words = []
 
-words = sorted(words, key=len, reverse=True)
-placed_words = []
-first_word = words[0]
-placed_words.append(first_word)
-filled_indexes = []
+    with open("crossword.json") as file: 
+        crossword = {}
+        crossword = json.load(file)
+        for word in crossword:
+            words.append(word)
 
-start_index = 13 - (len(first_word) // 2)
+    board = make(words)
+    print(board)
 
-for character in first_word:
-    board[13][start_index] = character
-    filled_indexes.append((13, start_index))
-    start_index += 1
+def make(words):
+    board = [[None for i in range(25)] for j in range(25)]
+    words = sorted(words, key=len, reverse=True)
+    placed_words = []
+    first_word = words[0]
+    placed_words.append(first_word)
+    filled_indexes = []
 
-print(board)
+    start_index = 13 - (len(first_word) // 2)
+
+    for character in first_word:
+        board[13][start_index] = character
+        filled_indexes.append((13, start_index))
+        start_index += 1
+    
+    return board
 
 
+def place(word, crossword, placement):
+    ...
+
+def can_place():
+    ...
+
+
+class Placement:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+
+
+if __name__ == "__main__":
+    main()
