d786fd126db0c5370b5ad2adf9d9df898a0ccb4b
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-05-09 20:27:55 +0530

    Wrote the property for direction & wrote place function

--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 import json
 
 
@@ -14,36 +15,48 @@ def main():
     print(board)
 
 def make(words):
+    max_words = len(words)
     board = [[None for i in range(25)] for j in range(25)]
     words = sorted(words, key=len, reverse=True)
     placed_words = []
-    first_word = words[0]
+    first_word = words.pop(0)
     placed_words.append(first_word)
-    filled_indexes = []
-
     start_index = 13 - (len(first_word) // 2)
-
-    for character in first_word:
-        board[13][start_index] = character
-        filled_indexes.append((13, start_index))
-        start_index += 1
-    
+    place(first_word, board, Placement(row=13, column=start_index, direction="h"))
     return board
 
 
-def place(word, crossword, placement):
-    ...
+def place(word, board, placement):
+    if placement.direction == "h":
+        shift = 0
+        for character in word:
+            board[placement.row][placement.column + shift] = character
+            shift += 1
+
+    elif placement.direction == "v":
+        shift = 0
+        for character in word:
+            board[placement.row + shift][placement.column] = character
 
 def can_place():
     ...
 
 
 class Placement:
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
+    def __init__(self, row, column, direction):
+        self.row = row
+        self.column = column  
         self.direction = direction
 
+    @property
+    def direction(self):
+        return self._direction
+    
+    @direction.setter
+    def direction(self, direction):
+        if direction not in ["h", "v"]:
+            raise ValueError("Invalid Direction")
+        self._direction = direction
 
 if __name__ == "__main__":
     main()
